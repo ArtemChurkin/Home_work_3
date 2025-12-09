b13a8c2c679b6b95eb9cd74d447b2d3a430b26ff
Artyom Churkin <a.churkin@informer.com>
2025-12-10 00:01:10 +0300

    added changes DemoqaHomeWork.java

--- Home_work_3.1/src/test/java/DemoqaHomeWork.java
+++ Home_work_3.1/src/test/java/DemoqaHomeWork.java
@@ -1,3 +1,4 @@
+
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -6,14 +7,13 @@ import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
-public class demoqaHomeWorkWithComments {
+public class DemoqaHomeWork {
 
     @BeforeAll
-    static void beforeAll() {
+    static void setupSelenideEnv() {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.holdBrowserOpen = true;
         Configuration.timeout = 5000;
     }
 
@@ -35,14 +35,7 @@ public class demoqaHomeWorkWithComments {
         $(".react-datepicker__day.react-datepicker__day--003:not(.react-datepicker__day--outside-month)").click();
         $("#dateOfBirthInput").shouldHave(value("03 Dec 1989"));
         $("#subjectsInput").setValue("Physics").pressEnter();
-        //$("#subjectsInput").scrollTo().setValue("Physics").pressEnter();
-        //$(".subjects-auto-complete__multi-value__label")
-                //.shouldHave(text("Physics"));
-        //$(".subjects-auto-complete__value-container.subjects-auto-complete__value-container--is-multi").click();
-        //$(".subjects-auto-complete__value-container.subjects-auto-complete__value-container--is-multi").setValue("Ph").pressEnter();
         $("label[for='hobbies-checkbox-3']").click();
-        //$("#hobbiesWrapper").$(byText("Music)).click();
-        //$("#uploadPicture").uploadFile(new File("Home_work_3.1/src/test/resources/img/123.png"));
         $("#uploadPicture").uploadFromClasspath("img/123.png");
         $("#state").click();
         $("#stateCity-wrapper").$(byText("Rajasthan")).click();
@@ -52,7 +45,9 @@ public class demoqaHomeWorkWithComments {
 
         $(".modal-dialog").should(appear);
         $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
-       // $(".table-responsive").shouldHave( text("Artyom"), text("a.churkin@informer.com"), text("89201171319"));
+        $(".table-responsive").shouldHave( text("Artyom"), text("a.churkin@informer.com"),
+                text("Male"), text("8920117131"),text("03 December,1989"),text("Physics")
+                ,text("Music"),text("123.png"),text("Svobody 78"),text("Rajasthan Jaipur"));
 
 
         sleep(5000L);
